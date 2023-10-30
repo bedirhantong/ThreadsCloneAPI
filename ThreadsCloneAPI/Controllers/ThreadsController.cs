13b5b330b3971625c1f87c0e842b788484589f35
Bedirhan Tong <bedirhantongdev@gmail.com>
2023-10-30 11:48:39 +0300

    Basic Http request are added w basic customization

--- ThreadsCloneAPI/Controllers/ThreadsController.cs
+++ ThreadsCloneAPI/Controllers/ThreadsController.cs
@@ -27,5 +27,72 @@ namespace ThreadsCloneAPI.Controllers
             }
             return Ok(thread);
         }
+
+
+
+        [HttpPost]
+        public IActionResult CreateOneThread([FromBody] Models.Thread thread)
+        {
+            try
+            {
+                if (thread is null)
+                {
+                    return BadRequest();
+                }
+
+                ApplicationContext.Threads.Add(thread);
+                return StatusCode(201, thread);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateOneThread([FromRoute(Name = "id")] int id, [FromBody] Models.Thread thread)
+        {
+            var entity = ApplicationContext.Threads.Find(b => b.Id.Equals(id));
+            if (entity is null)
+            {
+                return NotFound();
+            }
+
+            if (id != thread.Id)
+            {
+                return BadRequest("Given id and the thread id does not match!");
+            }
+
+            ApplicationContext.Threads.Remove(entity);
+
+            thread.Id = entity.Id;
+            ApplicationContext.Threads.Add(thread);
+
+            return Ok(thread);
+        }
+
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteOneThread([FromRoute] int id)
+        {
+            var thread = ApplicationContext.Threads.Find(b => b.Id.Equals(id));
+            if (thread is null)
+            {
+                return NotFound(new
+                {
+                    StatusCode = 404,
+                    message = $"Book with the {id} could not found"
+                });
+            }
+
+            ApplicationContext.Threads.Remove(thread);
+
+            return StatusCode(201, "Succesfully Removed");
+        }
+
+
+
+
     }
 }
--- ThreadsCloneAPI/Data/ApplicationContext.cs
+++ ThreadsCloneAPI/Data/ApplicationContext.cs
@@ -5,7 +5,7 @@ namespace ThreadsCloneAPI.Data
     {
         public static List<Models.Thread> Threads { get; set; }
         static ApplicationContext() { 
-            threads = new List<Models.Thread>()
+            Threads = new List<Models.Thread>()
             {
                 new Models.Thread() {Id = 1, WhoPosted = "bedirhantng", Content = "Hello World!"},
                 new Models.Thread() {Id = 2, WhoPosted = "serhanbymz", Content = "I am Flying"},
