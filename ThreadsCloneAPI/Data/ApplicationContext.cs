6b8a61be68e4b9b04f6a4b0233e9af5216be089b
Bedirhan Tong <bedirhantongdev@gmail.com>
2023-10-30 11:42:16 +0300

    Add project files.

---
+++ ThreadsCloneAPI/Data/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using ThreadsCloneAPI.Models;
+namespace ThreadsCloneAPI.Data
+{
+    public class ApplicationContext
+    {
+        public static List<Models.Thread> threads { get; set; }
+        static ApplicationContext() { 
+            threads = new List<Models.Thread>()
+            {
+                new Models.Thread() {Id = 1, WhoPosted = "bedirhantng", Content = "Hello World!"},
+                new Models.Thread() {Id = 2, WhoPosted = "serhanbymz", Content = "I am Flying"},
+            };
+        }
+    }
+}
---
+++ ThreadsCloneAPI/Models/Thread.cs
@@ -0,0 +1,9 @@
+﻿namespace ThreadsCloneAPI.Models
+{
+    public class Thread
+    {
+        public int Id { get; set; }
+        public String? WhoPosted { get; set; }
+        public String? Content { get; set; }
+    }
+}
